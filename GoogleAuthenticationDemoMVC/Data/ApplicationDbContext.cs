74eff989adecd8d9faec101ef5c243b0c4c1efdd
Kiziltan Cihan Oral <kiziltancihanoral@gmail.com>
2023-08-31 22:27:58 +0300

    Add project files.

---
+++ GoogleAuthenticationDemoMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoogleAuthenticationDemoMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
